23c94ed198f23905b3ea5e7f9487298afeb4e339
Jessica Orrick <jessica.orrick@outlook.com>
2023-09-28 14:16:10 -0500

    update after sync

--- arrays/exercises/part-six-arrays.js
+++ arrays/exercises/part-six-arrays.js
@@ -1,11 +1,37 @@
 //Arrays can hold different data types, even other arrays! A multi-dimensional array is one with entries that are themselves arrays.
 
 //1) Define and initialize the arrays specified in the exercise to hold the name, chemical symbol and mass for different elements.
+let element1 = ['hydrogen', 'H', 1.008];
+let element2 = ['helium', 'He', 4.003];
+let element26 = ['iron', 'Fe', 55.85];
 
 //2) Define the array 'table', and use 'push' to add each of the element arrays to it. Print 'table' to see its structure.
+let table = []
+table.push(element1) +table.push(element2)+table.push(element26);
+console.log(table);
 
 //3) Use bracket notation to examine the difference between printing 'table' with one index vs. two indices (table[][]).
-
+console.log(table[1]);
+console.log(table[0][0]);
 //4) Using bracket notation and the table array, print the mass of element1, the name for element 2 and the symbol for element26.
+console.log(table[0][2]);
+console.log(table[1][0]);
+console.log(table[2][1]);
 
 //5) 'table' is an example of a 2-dimensional array. The first “level” contains the element arrays, and the second level holds the name/symbol/mass values. Experiment! Create a 3-dimensional array and print out one entry from each level in the array.
+//This is from the book example of "Multi-level arrays". I do not quite undertand this yet. 
+let shuttleCrews = [
+    ['Robert Gibson', 'Mark Lee', 'Mae Jemison'],
+    ['Kent Rominger', 'Ellen Ochoa', 'Bernard Harris'],
+    ['Eilen Collins', 'Winston Scott',  'Catherin Coleman']
+];
+
+let newCrew = ['Mark Polansky', 'Robert Curbeam', 'Joan Higginbotham'];
+
+// Add a new crew array to the end of shuttleCrews
+shuttleCrews.push(newCrew);
+console.log(shuttleCrews[3][2]);
+
+// Reverse the order of the crew at index 1
+shuttleCrews[1].reverse();
+console.log(shuttleCrews[1]);

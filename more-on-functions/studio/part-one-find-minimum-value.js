9742d1d64f54956631bfb8a5db332dd54bbaecfb
Jessica Orrick <jessica.orrick@outlook.com>
2023-09-30 21:09:05 -0500

    part 1 studio

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -5,6 +5,16 @@ let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
+let ants = function (arrayOfNums) {
+    let smallest = arrayOfNums[0];
+    for (i = 0; i < arrayOfNums.length; i++) {
+            if (arrayOfNums[i] < smallest) {
+                smallest = arrayOfNums[i];
+            }
+        }
+        return smallest;
+    }
+
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(ants(nums2));

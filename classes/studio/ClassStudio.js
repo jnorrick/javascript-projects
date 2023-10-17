e12fe8625ce51b94fe844a2836b42015d466a456
Jessica Orrick <jessica.orrick@outlook.com>
2023-10-17 14:52:09 -0500

    completed through part 3

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,7 +1,63 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
 
+class CrewCandidate {
+    constructor(name, mass, scores) {
 
+        this.name = name; //string
+        this.mass = mass; //number
+        this.scores = scores // array of numbers
+    }
 
+    addScore(score) {
+        this.scores.push(score)
+    }
+
+    average() {
+        let totalSum = 0
+        let numCount = this.scores.length  
+        
+        for (let i in this.scores){
+            totalSum += this.scores[i];
+            
+        }
+        let crewMemberAverage = Math.round(totalSum/numCount*10)/10
+        this.averageScore = crewMemberAverage
+        return crewMemberAverage
+        
+    }
+
+    status() {
+        let crewStatus = ""
+        if (this.averageScore >= 90) {
+            crewStatus = "Accepted";
+        } else if (this.averageScore <= 89 && this.averageScore >= 80 ) {
+            crewStatus = "Reserve";
+        } else if (this.averageScore <= 79 && this.averageScore >= 70) {
+            crewStatus = "Probationary";
+        } else {
+            crewStatus = "Rejected"
+        } this.crewStatus = crewStatus 
+        return crewStatus 
+        
+    } 
+}
+
+let bubbaBear = new CrewCandidate("Bubba Bear", 135,[88,85,90])
+
+let merryMaltese = new CrewCandidate("Merry Maltese", 1.5, [93, 88, 97])
+
+let gladGator = new CrewCandidate("Glad Gator", 225, [75, 78, 62])
+
+// let crewResult = console.log(`${this.name} earned an average test score of ${this.averageScore} and has the status of ${this.crewStatus}`)
+// console.log(bubbaBear)
+// console.log(merryMaltese)
+// console.log(gladGator)
+
+// bubbaBear.addScore(83)
+merryMaltese.average(merryMaltese.scores)
+merryMaltese.status(merryMaltese.averageScore)
+console.log(merryMaltese)
+console.log(`${merryMaltese.name} earned an average test score of ${merryMaltese.averageScore} and has the status of ${merryMaltese.crewStatus}`)
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
 
